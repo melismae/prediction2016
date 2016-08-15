ea6073c1346070813ce085e0760b001676b56fa8
Melissa Smith <melismae@gmail.com>
2016-08-15 11:46:08 -0700

    param / function name changes

--- src/js/shared/components/Home.jsx
+++ src/js/shared/components/Home.jsx
@@ -3,16 +3,16 @@ import Dropdown from './Dropdown';
 
 export default class Home extends Component {
 
-    para(source, winning, losing, candidates) {
+    renderParagraph(source, winningParty, losingParty, candidates) {
         let url = source.url;
         let sourceName = source.sourceName;
-        let winnerName = `${candidates[winning].firstname} ${candidates[winning].lastname}`;
-        let loserName = `${candidates[losing].firstname} ${candidates[losing].lastname}`;
-        let winPercent = source[winning];
-        let losePercent = source[losing];
+        let winnerName = `${candidates[winningParty].firstname} ${candidates[winningParty].lastname}`;
+        let loserName = `${candidates[losingParty].firstname} ${candidates[losingParty].lastname}`;
+        let winPercent = source[winningParty];
+        let losePercent = source[losingParty];
 
         return(<p>
-            According to <a href={url} target="_blank" className={winning}>{sourceName}</a>, {winnerName} has a {winPercent}% chance of winning, while {loserName} has a {losePercent}% chance of winning.
+            According to <a href={url} target="_blank" className={winningParty}>{sourceName}</a>, {winnerName} has a {winPercent}% chance of winning, while {loserName} has a {losePercent}% chance of winning.
         </p>);
     }
 
@@ -34,7 +34,7 @@ export default class Home extends Component {
                 <div id="selected-text">
                     <p>&#8212;Get ready for&#8212;</p>
                     <h1 className={winningParty}>{president}</h1>
-                    {this.para(source, winningParty, losingParty, candidates)}
+                    {this.renderParagraph(source, winningParty, losingParty, candidates)}
                     <p>Don't like the chances? <a href="https://vote.usa.gov/" target="_blank" className={winningParty}>Register</a>, then <a href="https://gttp.votinginfoproject.org/" target="_blank" className={winningParty}>get out and vote</a> on November 8th!</p>
                 </div>
             </div>
